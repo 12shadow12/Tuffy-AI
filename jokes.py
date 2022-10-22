f62c9288463f63f14316b57912f0bc78b51a5a55
Donald Novasky <dnovasky@hotmail.com>
2022-10-21 22:47:02 -0700

    fixed issue with jokes.py

--- jokes.py
+++ jokes.py
@@ -5,13 +5,14 @@ import requests
 url = 'https://v2.jokeapi.dev/joke/'
 #headers = {'content-type': 'application/json',}
 
-joke = []
-def getJoke(type='Any'):
+def getJoke(type: str='Any'):
     response = requests.get(url + type).json()
     if not response['error']:
+        joke = []
         if response['type'] == 'single':
             joke.append('single')
             joke.append(response['joke'])
         elif response['type'] == 'twopart':
             joke.append(response['setup'])
             joke.append(response['delivery'])
+    return joke
